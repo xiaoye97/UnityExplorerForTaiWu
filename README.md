7d882abb513afd4f09e1088e3df262e239a8997c
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2022-09-11 06:03:30 +0800

    上传

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # UnityExplorerForTaiWu
-用于太吾绘卷的UnityExplorer加载器。UnityExplorer loader for The Scroll Of Taiwu.
+用于太吾绘卷的[UnityExplorer][1]加载器。[UnityExplorer][1] loader for The Scroll Of Taiwu.
+
+## 使用方法
+1. 下载[releases][2]中的最新版本
+2. 将Mod解压到游戏根目录下的Mod文件夹中(不要改文件夹的名字)
+3. 解压后的文件路径应该看起来像这样(太吾绘卷 The Scroll Of Taiwu 功能测试\Mod\UnityExplorerForTaiWu)
+4. 在游戏中启用此Mod
+
+[1]: https://github.com/sinai-dev/UnityExplorer
+[2]: https://github.com/xiaoye97/UnityExplorerForTaiWu/releases
---
+++ UnityExplorerForTaiWu/UnityExplorerForTaiWu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FrameWork;
+using HarmonyLib;
+using System.Reflection;
+using TaiwuModdingLib.Core.Plugin;
+
+namespace UnityExplorerForTaiWu
+{
+    [PluginConfig("UnityExplorerForTaiWu", "宵夜97", "1.0.0")]
+    public class UnityExplorerForTaiWu : TaiwuRemakePlugin
+    {
+        public override void Dispose()
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+            try
+            {
+                LoadDll("UniverseLib.Mono");
+                LoadDll("UnityExplorer.STANDALONE.Mono");
+                var type = AccessTools.TypeByName("UnityExplorer.ExplorerStandalone");
+                Traverse.Create(type).Method("CreateInstance").GetValue();
+            }
+            catch (Exception ex)
+            {
+                DialogCmd cmd = new DialogCmd
+                {
+                    Title = "异常",
+                    Content = $"加载UnityExplorer时出现了异常，请在Log文件查看详细信息。",
+                    Type = 2,
+                    GroupYesText = "好的"
+                };
+                UIElement.Dialog.SetOnInitArgs(EasyPool.Get<ArgumentBox>().SetObject("Cmd", cmd));
+                UIManager.Instance.ShowUI(UIElement.Dialog);
+                GLog.Error($"加载UnityExplorer时出现了异常:\n{ex}");
+            }
+        }
+
+        public Assembly LoadDll(string dllName)
+        {
+            string dllDirPath = ModManager.GetModRootFolder() + "/UnityExplorerForTaiWu/Plugins";
+            string dllPath = dllDirPath + "/" + dllName + ".dll";
+            var bytes = System.IO.File.ReadAllBytes(dllPath);
+            var a = Assembly.Load(bytes);
+            return a;
+        }
+    }
+}
